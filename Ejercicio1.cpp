f42e7f8237359e081acc6d556f13b6698d441a02
Fernandouca <62618452+Fernandouca@users.noreply.github.com>
2020-06-27 12:18:58 -0600

    Add files via upload

---
+++ Ejercicio1.cpp
@@ -0,0 +1,45 @@
+#include "iostream"
+#include "string"
+
+using namespace std;
+char cifrar(int, string);
+
+int mian()  
+{
+    //variables
+    string frase, cifrado;
+    char str[100];
+    //input - output
+    cout << "programa que codificara la clave mulcielago." << endl;
+    cout << "------ " << endl;
+    cout << "Ingrese la frase a codificar: " << endl;
+    cin.getline(str, 100, '\n');
+    frase = str;
+    //procedimiento 
+    for(int i = 0; i < frase.length(); i++)
+    {
+        cifrado = cifrado + cifrar(i, frase);
+    } 
+    cout << "------ " << endl;
+    cout << "La frase introducida es: " << frase << endl;
+    cout << "La frase cifrada es: " << cifrado << endl;
+
+    return 0;
+}
+
+char cifrar(int i, string frase)
+{
+    char aux = frase[i];
+    //claves
+    char clave1 [10] = {'m', 'u', 'r', 'c', 'i', 'e', 'l', 'a', 'g', 'o'};
+    char clave2 [10] = {'0', '1', '2', '3', '5', '6', '7', '8', '9'};
+    //procedimiento de cifrado 
+    for(int j = 0; j < 10; j++)
+    {
+        if(aux == clave1 [j])
+        {
+            return clave2 [j];
+        }
+    }
+    return aux;
+}
